c3ca67324a1b55708914829b26cccce2ada118ea
RealWarm <wjdgns8946@naver.com>
2021-06-09 17:40:14 +0900

    32강 완성

--- src/test/java/com/cen/controller/MemberTests.java
+++ src/test/java/com/cen/controller/MemberTests.java
@@ -1,6 +1,8 @@
 package com.cen.controller;
 
-import javax.inject.Inject;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 import javax.sql.DataSource;
 
 import org.junit.Test;
@@ -18,11 +20,9 @@ import lombok.extern.log4j.Log4j;
 		"file:src/main/webapp/WEB-INF/spring/security-context.xml"})
 public class MemberTests {
 
-//	@Inject
 	@Autowired
 	private PasswordEncoder pwencoder;
 	
-//	@Inject
 	@Autowired
 	private DataSource ds;
 	
@@ -30,15 +30,85 @@ public class MemberTests {
 	@Test
 	public void testIsertMember() {
 		
-//		System.out.println("PasswordEncoder :: " + pwencoder);
+		String sql = "insert into tbl_member(userid, userpw, username) values (?, ?, ?)";
+		
 		for(int i=0; i<100; i++) {
-			System.out.println(i + " : " + pwencoder.encode("pw"+i));
+			
+			Connection con = null;
+			PreparedStatement pstmt = null;
+			
+			try {
+				con = ds.getConnection();
+				pstmt = con.prepareStatement(sql);
+				
+				pstmt.setString(2, pwencoder.encode("pw" + i));
+				
+				if(i<80) {
+					pstmt.setString(1, "user"+i);
+					pstmt.setString(3, "일반사용자"+i);
+				}else if(i<90) {
+					pstmt.setString(1, "manager"+i);
+					pstmt.setString(3, "운영자"+i);
+				}else {
+					pstmt.setString(1, "admin"+i);
+					pstmt.setString(3, "관리자"+i);
+				}//if
+				
+				pstmt.executeUpdate();
+				
+			}catch(Exception e) {
+				e.printStackTrace();
+			}finally {
+				if(pstmt != null) {try {pstmt.close();}catch(Exception e) {}}
+				if(con != null) {try {con.close();}catch(Exception e) {}}
+			}//try-catch
+			
 		}//for-i
 		
 	}//testIsertMember
 	
+	@Test
+	public void testInsetAuth() {
+		
+		String sql = "insert into tbl_member_auth (userid, auth) values (?, ?)";
+		
+		for(int i=0; i<100; i++) {
+			
+			Connection con = null;
+			PreparedStatement pstmt = null;
+			
+			try {
+				con = ds.getConnection();
+				pstmt = con.prepareStatement(sql);
+								
+				if(i<80) {
+					pstmt.setString(1, "user"+i);
+					pstmt.setString(2, "ROLE_USER");
+				}else if(i<90) {
+					pstmt.setString(1, "manager"+i);
+					pstmt.setString(2, "ROLE_MEMBER");
+				}else {
+					pstmt.setString(1, "admin"+i);
+					pstmt.setString(2, "ROLE_ADMIN");
+				}//if
+				
+				pstmt.executeUpdate();
+				
+			}catch(Exception e) {
+				e.printStackTrace();
+			}finally {
+				if(pstmt != null) {try {pstmt.close();}catch(Exception e) {}}
+				if(con != null) {try {con.close();}catch(Exception e) {}}
+			}//try-catch
+			
+		}//for-i
+		
+		
+	}//testInsetAuth
+	
 	
 }//end class
+
 
 
 
